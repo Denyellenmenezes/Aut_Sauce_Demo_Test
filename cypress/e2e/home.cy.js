0beeeaa9e7cb86d222c5e03b9c4031e7dc986ad9
Denyellen Menezes <menezesdenyellen@gmail.com>
2025-01-29 20:58:19 -0300

    Adição de contexto ao arquivo de teste home

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,8 +1,12 @@
 //Suite de testes
 
-describe('Home', () => {
+describe('Demo Page', () => {
+  context('Access Website ', ()=> {
+    it('Access Sauce Demo Page', () => {
+      cy.title().should('eq', 'Swag Labs')
+ })
 
-  it('Access Sauce Demo Page', () => {
-       cy.title().should('eq', 'Swag Labs')
   })
+
+  
 })
